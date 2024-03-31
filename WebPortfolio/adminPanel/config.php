3d7265ecdb5107c0f0757c9b05e0b8ad35909c14
Bilal Aqeel <sheikhbilal1025@gmail.com>
2024-03-31 16:31:38 +0500

    Delete WebPortfolio directory

--- WebPortfolio/adminPanel/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server = "localhost";
-    $username = "root";
-    $pass = "";
-    $database = "newportfolio";
-
-    $conn = mysqli_connect( "$server", "$username", "$pass","$database" ) or die("Connection Failed");
-?>
--- WebPortfolio/adminPanel/sessionStart.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "config.php";
-    
-    session_start();
-    $_SESSION['username'] = "admin"; //assigning username to the session variable
-    $_SESSION['pwd'] = "admin"; 
-?>
